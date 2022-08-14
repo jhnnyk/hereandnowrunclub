6c8f0a3a1b66150fca07d8b07766dbc1f2cbbb17
John Knotts <jhnnyk@gmail.com>
2022-08-14 12:51:08 -0600

    initial header styling and images

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
+  <NavBar />
 
   <RouterView />
 </template>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Quicksand', sans-serif;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <header>
+    <RouterLink to="/">
+      <img
+        src="../assets/images/logo.png"
+        alt="Here and Now Run Club"
+        id="logo"
+      />
+    </RouterLink>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/coaches">Coaches</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  background: url('../assets/images/DSC02505.jpg') no-repeat center 85%;
+  background-size: cover;
+  padding: 2% 2% 12% 2%;
+  display: flex;
+  justify-content: space-between;
+}
+
+header a {
+  color: white;
+}
+
+#logo {
+  width: 230px;
+  height: 230px;
+}
+</style>
